86c602167792a09d35824d789a6fcc334c7eb601
João Paulo Dias <joaopdias.dev@gmail.com>
2025-09-29 10:53:38 -0300

    fix: enable cors

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,8 +1,18 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	return r
 }
